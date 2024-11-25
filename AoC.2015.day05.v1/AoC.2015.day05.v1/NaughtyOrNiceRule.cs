ad99b5a43d7ffe70014c93894243554a567d3a02
Reuben Hillyer <reuben.hillyer@madetech.com>
2024-11-25 14:19:00 +0000

    We know that a string with any repeating letter can be nice

--- AoC.2015.day05.v1/AoC.2015.day05.v1/NaughtyOrNiceRule.cs
+++ AoC.2015.day05.v1/AoC.2015.day05.v1/NaughtyOrNiceRule.cs
@@ -4,12 +4,19 @@ public class NaughtyOrNiceRule
 {
     public bool IsNice(string initialInput)
     {
-        List<string> mustContain = ["aa", "bb"];
-        if (mustContain.Any(initialInput.Contains))
+        if (ContainsDoubleLetter(initialInput))
         {
             return true;
         }
 
         return false;
+    }
+
+    private static bool ContainsDoubleLetter(string initialInput)
+    {
+        List<string> mustContain = ["aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm",
+            "nn", "oo", "pp", "qq", "rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz"];
+        
+        return mustContain.Any(initialInput.Contains);
     }
 }
