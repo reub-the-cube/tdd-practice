37b1d24a86b13a17d551bccafac6eb18d6028598
Reuben <reuben.hillyer@hotmail.co.uk>
2024-11-26 21:52:08 +0000

    Add acceptance test for part two checker from examples given

--- AoC.2015.day05.v1/AoC.2015.day05.v1.Tests/SantaStringCheckerPartTwoTests.cs
+++ AoC.2015.day05.v1/AoC.2015.day05.v1.Tests/SantaStringCheckerPartTwoTests.cs
@@ -18,12 +18,15 @@ namespace AoC._2015.day05.v1.Tests
             isNice.Should().Be(false);
         }
 
-        //[Theory]
-        //[InlineData("abcab")] // Repeated ab, no pair of characters with a letter between them
-        //public void InputThatMeetsOneRuleReturnsFalseForIsValid(string initialInput)
-        //{
+        [Theory]
+        [InlineData("abcab")] // Repeated ab, no pair of characters with a letter between them
+        [InlineData("acab")] // No repeated pair, a pair of characters with a letter between them
+        public void InputThatMeetsOneRuleReturnsFalseForIsValid(string initialInput)
+        {
+            var isNice = _santaStringChecker.IsNice(initialInput);
 
-        //}
+            isNice.Should().Be(false);
+        }
 
         [Theory]
         [InlineData("abab")]
@@ -32,6 +35,26 @@ namespace AoC._2015.day05.v1.Tests
             var isNice = _santaStringChecker.IsNice(initialInput);
 
             isNice.Should().Be(true);
+        }
+
+        [Theory]
+        [InlineData("qjhvhtzxzqqjkmpb")]
+        [InlineData("xxyxx")]
+        public void ExamplesReturnTrueForIsValid(string initialValue)
+        {
+            var isNice = _santaStringChecker.IsNice(initialValue);
+
+            isNice.Should().Be(true);
+        }
+
+        [Theory]
+        [InlineData("uurcxstgmygtbstg")]
+        [InlineData("ieodomkazucvgmuy")]
+        public void ExamplesReturnFalseForIsValid(string initialValue)
+        {
+            var isNice = _santaStringChecker.IsNice(initialValue);
+
+            isNice.Should().Be(false);
         }
     }
 }
