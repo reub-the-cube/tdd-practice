78ef90f5cc600b048cb02f264ccc8ec078681705
Reuben <reuben.hillyer@hotmail.co.uk>
2024-11-26 20:31:36 +0000

    We can tell if a string has two or more occurences of a character pair

---
+++ AoC.2015.day05.v1/AoC.2015.day05.v1.Tests/RepeatingStringWithoutOverlapRuleTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+
+namespace AoC._2015.day05.v1.Tests
+{
+    public class RepeatingStringWithoutOverlapRuleTests
+    {
+        [Fact]
+        public void EmptyStringReturnsFalseForIsNice()
+        {
+            var rule = new RepeatingStringWithoutOverlapRule();
+
+            var isNice = rule.IsNice(string.Empty);
+
+            isNice.Should().Be(false);
+        }
+
+        [Theory]
+        [InlineData("abab")]
+        [InlineData("abaab")]
+        [InlineData("acac")]
+        public void RepeatingSubstringReturnsTrueForIsNice(string initialInput)
+        {
+            var rule = new RepeatingStringWithoutOverlapRule();
+
+            var isNice = rule.IsNice(initialInput);
+
+            isNice.Should().Be(true);
+        }
+
+        [Theory]
+        [InlineData("aba")]
+        [InlineData("abba")]
+        public void NonRepeatingSubstringReturnsFalseForIsNice(string initialInput)
+        {
+            var rule = new RepeatingStringWithoutOverlapRule();
+
+            var isNice = rule.IsNice(initialInput);
+
+            isNice.Should().Be(false);
+        }
+    }
+}
---
+++ AoC.2015.day05.v1/AoC.2015.day05.v1/RepeatingStringWithoutOverlapRule.cs
@@ -0,0 +1,33 @@
+﻿namespace AoC._2015.day05.v1
+{
+    public class RepeatingStringWithoutOverlapRule : StringRule
+    {
+        public override bool IsNice(string initialInput)
+        {
+            if (initialInput == string.Empty)
+            {
+                return false;
+            }
+
+            var stringPairs = CreatePairs(initialInput);
+            if (stringPairs.Any(pair => initialInput.Split(pair).Length > 2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static List<string> CreatePairs(string input)
+        {
+            List<string> pairs = [];
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                pairs.Add(input.Substring(i, 2));
+            }
+
+            return pairs;
+        }
+    }
+}
