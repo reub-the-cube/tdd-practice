9c894c1e5e16b8576736c9a89e331592dd02ac96
Reuben Hillyer <reuben.hillyer@madetech.com>
2024-11-28 08:19:47 +0000

    We can return all positions between two if they're on the same X coordinate

--- AoC.2015.day06.v1/AoC.2015.day06.v1.Tests/PositionTests.cs
+++ AoC.2015.day06.v1/AoC.2015.day06.v1.Tests/PositionTests.cs
@@ -5,7 +5,7 @@ namespace AoC._2015.day06.v1.Tests;
 public class PositionTests
 {
     [Fact]
-    public void GetPositionsBetweenReturnsOnePointForTheSamePoint()
+    public void GetPositionsBetweenTheSameStartAndEndPositionIsASinglePosition()
     {
         var here = new Position(1, 1);
         var there = here;
@@ -13,5 +13,18 @@ public class PositionTests
         var positions = Position.GetPositionsBetween(here, there);
 
         positions.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public void GetPositionsBetweenTwoPositionsOnTheSameRowIncludesEveryPositionBetweenAndInclusive()
+    {
+        var here = new Position(1, 1);
+        var there = new Position(1, 10);
+
+        var positions = Position.GetPositionsBetween(here, there);
+
+        positions.Should().HaveCount(10);
+        positions.Should().Contain(here);
+        positions.Should().Contain(there);
     }
 }
--- AoC.2015.day06.v1/AoC.2015.day06.v1/Position.cs
+++ AoC.2015.day06.v1/AoC.2015.day06.v1/Position.cs
@@ -2,8 +2,15 @@ namespace AoC._2015.day06.v1;
 
 public record Position(int X, int Y)
 {
-    public static IEnumerable<Position> GetPositionsBetween(Position here, Position there)
+    public static HashSet<Position> GetPositionsBetween(Position here, Position there)
     {
-        yield return here;
+        var positions = new HashSet<Position>();
+
+        for (int y = here.Y; y < there.Y + 1; y++)
+        {
+            positions.Add(new Position(here.X, y));
+        }
+
+        return positions;
     }
 }
