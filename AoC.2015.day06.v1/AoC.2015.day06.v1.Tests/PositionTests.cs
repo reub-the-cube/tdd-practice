834ecd2419041e06ad0c071bbe2da64c6178776f
Reuben Hillyer <reuben.hillyer@madetech.com>
2024-11-28 18:39:47 +0000

    We can get all positions in a grid and know that the first parameter is the bottom left corner

--- AoC.2015.day06.v1/AoC.2015.day06.v1.Tests/PositionTests.cs
+++ AoC.2015.day06.v1/AoC.2015.day06.v1.Tests/PositionTests.cs
@@ -4,6 +4,19 @@ namespace AoC._2015.day06.v1.Tests;
 
 public class PositionTests
 {
+    [Theory]
+    [InlineData(1, 0)]
+    [InlineData(0, 1)]
+    public void GetPositionsBetweenThrowsExceptionIfFromPositionIsNotBottomLeft(int thereX, int thereY)
+    {
+        var here = new Position(1, 1);
+        var there = new Position(thereX, thereY);
+
+        HashSet<Position> positions() => Position.GetPositionsBetween(here, there);
+
+        Assert.Throws<ArgumentException>((Func<HashSet<Position>>)positions);
+    }
+
     [Fact]
     public void GetPositionsBetweenTheSameStartAndEndPositionIsASinglePosition()
     {
@@ -26,5 +39,33 @@ public class PositionTests
         positions.Should().HaveCount(10);
         positions.Should().Contain(here);
         positions.Should().Contain(there);
+    }
+
+    [Fact]
+    public void GetPositionsBetweenTwoPositionsOnTheSameColumnIncludesEveryPositionBetweenAndInclusive()
+    {
+        var here = new Position(1, 1);
+        var there = new Position(10, 1);
+
+        var positions = Position.GetPositionsBetween(here, there);
+
+        positions.Should().HaveCount(10);
+        positions.Should().Contain(here);
+        positions.Should().Contain(there);
+    }
+
+    [Fact]
+    public void GetPositionsBetweenTwoPositionsOnSquareIncludesAllFourCorners()
+    {
+        var here = new Position(1, 1);
+        var there = new Position(10, 10);
+
+        var positions = Position.GetPositionsBetween(here, there);
+
+        positions.Should().HaveCount(100);
+        positions.Should().Contain(here);
+        positions.Should().Contain(there);
+        positions.Should().Contain(new Position(1, 10));
+        positions.Should().Contain(new Position(10, 1));
     }
 }
--- AoC.2015.day06.v1/AoC.2015.day06.v1/Position.cs
+++ AoC.2015.day06.v1/AoC.2015.day06.v1/Position.cs
@@ -4,13 +4,26 @@ public record Position(int X, int Y)
 {
     public static HashSet<Position> GetPositionsBetween(Position here, Position there)
     {
-        var positions = new HashSet<Position>();
-
-        for (int y = here.Y; y < there.Y + 1; y++)
+        if (here.X > there.X || here.Y > there.Y)
         {
-            positions.Add(new Position(here.X, y));
+            throw new ArgumentException("Should be bottom-left position", nameof(here));
         }
 
-        return positions;
+        IEnumerable<Position> positions = [];
+
+        for (int x = here.X; x < there.X + 1; x++)
+        {
+            positions = positions.Union(GetColumnPositions(x, here.Y, there.Y));
+        }
+
+        return positions.ToHashSet();
+    }
+
+    private static IEnumerable<Position> GetColumnPositions(int x, int fromY, int toY)
+    {
+        for (int y = fromY; y < toY + 1; y++)
+        {
+            yield return new Position(x, y);
+        }
     }
 }
