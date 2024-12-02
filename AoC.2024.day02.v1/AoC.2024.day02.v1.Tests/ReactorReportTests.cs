f9cc20b643d277eeeda6b3ac04cf0ff39b9c968a
Reuben <reuben.hillyer@hotmail.co.uk>
2024-12-02 21:37:34 +0000

    We can tell if an individual reactor report is safe

---
+++ AoC.2024.day02.v1/AoC.2024.day02.v1.Tests/ReactorReportTests.cs
@@ -0,0 +1,110 @@
+﻿using FluentAssertions;
+
+namespace AoC._2024.day02.v1.Tests
+{
+    public class ReactorReportTests
+    {
+        [Fact]
+        public void AnEmptyReportIsSafe()
+        {
+            var report = new ReactorReport([]);
+
+            var isSafe = report.IsSafe();
+
+            isSafe.Should().Be(true);
+        }
+
+        [Fact]
+        public void AReportWithTwoEqualAdjacentNumbersIsNotSafe()
+        {
+            var report = new ReactorReport(["3", "3"]);
+
+            var isSafe = report.IsSafe();
+
+            isSafe.Should().Be(false);
+        }
+
+        [Fact]
+        public void AReportWithTwoDifferentNumbersThatAreIncreasingASmallAmountIsSafe()
+        {
+            var report = new ReactorReport(["3", "4"]);
+
+            var isSafe = report.IsSafe();
+
+            isSafe.Should().Be(true);
+        }
+
+        [Fact]
+        public void AReportWithOneNumberIsSafe()
+        {
+            var report = new ReactorReport(["3"]);
+
+            var isSafe = report.IsSafe();
+
+            isSafe.Should().Be(true);
+        }
+
+        [Fact]
+        public void AReportWithTwoDifferentNumbersThatAreIncreasingALargeAmountIsNotSafe()
+        {
+            var report = new ReactorReport(["3", "7"]);
+
+            var isSafe = report.IsSafe();
+
+            isSafe.Should().Be(false);
+        }
+
+        [Fact]
+        public void AReportWithThreeDifferentNumbersThatIncreaseCloseAndThenFarApartIsNotSafe()
+        {
+            var report = new ReactorReport(["3", "4", "8"]);
+
+            var isSafe = report.IsSafe();
+
+            isSafe.Should().Be(false);
+        }
+
+        [Fact]
+        public void AReportWithThreeDifferentNumbersThatIncreasesAndDecreasesIsNotSafe()
+        {
+            var report = new ReactorReport(["3", "4", "3"]);
+
+            var isSafe = report.IsSafe();
+
+            isSafe.Should().Be(false);
+        }
+
+        [Fact]
+        public void AReportWithTwoDifferentNumbersThatDecreaseASmallAmountIsSafe()
+        {
+            var report = new ReactorReport(["4", "3"]);
+
+            var isSafe = report.IsSafe();
+
+            isSafe.Should().Be(true);
+        }
+
+        [Fact]
+        public void AReportWithTwoDifferentNumbersThatAreDecreasingALargeAmountIsNotSafe()
+        {
+            var report = new ReactorReport(["7", "3"]);
+
+            var isSafe = report.IsSafe();
+
+            isSafe.Should().Be(false);
+        }
+
+
+        [Fact]
+        public void AReportWithThreeDifferentNumbersThatDecreasesAndIncreasesIsNotSafe()
+        {
+            var report = new ReactorReport(["4", "3", "4"]);
+
+            var isSafe = report.IsSafe();
+
+            isSafe.Should().Be(false);
+        }
+    }
+}
+
+// Decreasing numbers
---
+++ AoC.2024.day02.v1/AoC.2024.day02.v1/ReactorReport.cs
@@ -0,0 +1,83 @@
+﻿namespace AoC._2024.day02.v1
+{
+    public class ReactorReport(List<string> reportInput)
+    {
+        private const int safeGap = 3;
+        private readonly List<int> _reportInput = reportInput.Select(int.Parse).ToList();
+        private LevelDirection _levelDirection = LevelDirection.NotSet;
+
+        public bool IsSafe()
+        {
+            SetInitialLevelDirection();
+            return IsStillSafe();
+        }
+
+        private void SetInitialLevelDirection()
+        {
+            if (_reportInput.Count > 1)
+            {
+                if (_reportInput[0] > _reportInput[1])
+                {
+                    _levelDirection = LevelDirection.EverDecreasing;
+                }
+                else if (_reportInput[0] < _reportInput[1])
+                {
+                    _levelDirection = LevelDirection.EverIncreasing;
+                }
+            }
+        }
+
+        private bool IsStillSafe()
+        {
+            if (_reportInput.Count < 2)
+            {
+                return true;
+            }
+
+            if (!IsNextNumberSafe())
+            {
+                return false;
+            }
+
+            _reportInput.RemoveAt(0);
+            return IsStillSafe();
+        }
+
+        private bool IsNextNumberSafe()
+        {
+            if (_reportInput[0] == _reportInput[1])
+            {
+                return false;
+            }
+
+            if (_reportInput[0] + safeGap < _reportInput[1])
+            {
+                return false;
+            }
+
+            if (_reportInput[0] - safeGap > _reportInput[1])
+            {
+                return false;
+            }
+
+            if (_reportInput[0] > _reportInput[1] && _levelDirection == LevelDirection.EverIncreasing)
+            {
+                return false;
+            }
+
+            if (_reportInput[0] < _reportInput[1] && _levelDirection == LevelDirection.EverDecreasing)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private enum LevelDirection
+        {
+            NotSet,
+            EverIncreasing,
+            EverDecreasing
+        }
+    }
+}
