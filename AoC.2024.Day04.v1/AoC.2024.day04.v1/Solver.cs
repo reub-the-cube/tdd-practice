b1b4c387fe82177cc65756b77c393e6079c5bb8e
Reuben <reuben.hillyer@hotmail.co.uk>
2024-12-04 23:36:30 +0000

    Introduce a solver class for scanning an enumerable of string for a given specific word

---
+++ AoC.2024.Day04.v1/AoC.2024.day04.v1/Solver.cs
@@ -0,0 +1,21 @@
+﻿namespace AoC._2024.day04.v1
+{
+    public class Solver(string word)
+    {
+        private string ReversedWord => new(word.Reverse().ToArray());
+
+        public int CountOccurrencesIn(IEnumerable<string> checkAgainst)
+        {
+            return checkAgainst.Sum(Scan);
+        }
+
+        private int Scan(string text)
+        {
+            var startIndexes = Enumerable.Range(0, text.Length - word.Length + 1);
+            var allSubstrings = startIndexes.Select(s => text.Substring(s, word.Length));
+            var numberOfOccurrences = allSubstrings.Count(s => s == word || s == ReversedWord);
+
+            return numberOfOccurrences;
+        }
+    }
+}
--- AoC.2024.Day04.v1/AoC.2024.day04.v1/WordSearch.cs
+++ AoC.2024.Day04.v1/AoC.2024.day04.v1/WordSearch.cs
@@ -4,55 +4,46 @@
     {
         public int FindOccurrencesOf(string word)
         {
-            int rowOccurrences = CountOccurrencesIn(letterDistribution, word);
-            int columnOccurrences = CountOccurrencesIn(GetColumns(letterDistribution), word);
-            int diagonalOccurences = CountOccurrencesIn(GetDiagonals(word), word);
+            var solver = new Solver(word);
+
+            int rowOccurrences = solver.CountOccurrencesIn(letterDistribution);
+            int columnOccurrences = solver.CountOccurrencesIn(GetColumns());
+            int diagonalOccurences = solver.CountOccurrencesIn(GetDiagonals(word.Length));
 
             return rowOccurrences + columnOccurrences + diagonalOccurences;
         }
 
-        private static int CountOccurrencesIn(IEnumerable<string> checkAgainst, string word)
+        private IEnumerable<string> GetColumns()
         {
-            return checkAgainst.Sum(c => Scan(c, word));
+            return Transpose(letterDistribution);
         }
 
-        private static IEnumerable<string> GetColumns(IEnumerable<string> rows)
+        private List<string> GetDiagonals(int length)
         {
-            IEnumerable<int> columnIndexes = Enumerable.Range(0, rows.First().Length);
-            return columnIndexes.Select(c => new string(rows.Select(l => l[c]).ToArray()));
-        }
-
-        private List<string> GetDiagonals(string word)
-        {
-            var rowsIndexesWithDiagonals = Enumerable.Range(0, letterDistribution.Length - word.Length + 1);
-            var downAndRightDiagonals = rowsIndexesWithDiagonals.SelectMany(row => GetDiagonalsForRow(row, word, true)).ToList();
-            var downAndLeftDiagonals = rowsIndexesWithDiagonals.SelectMany(row => GetDiagonalsForRow(row, word, false)).ToList();
+            var rowsIndexesWithDiagonals = Enumerable.Range(0, letterDistribution.Length - length + 1);
+            var downAndRightDiagonals = rowsIndexesWithDiagonals.SelectMany(row => GetDiagonalsForRow(row, length, true)).ToList();
+            var downAndLeftDiagonals = rowsIndexesWithDiagonals.SelectMany(row => GetDiagonalsForRow(row, length, false)).ToList();
 
             downAndRightDiagonals.AddRange(downAndLeftDiagonals);
             return downAndRightDiagonals;
         }
 
-        private IEnumerable<string> GetDiagonalsForRow(int row, string word, bool outFromTop)
+        private IEnumerable<string> GetDiagonalsForRow(int row, int length, bool outFromTop)
         {
-            var offsetValues = Enumerable.Range(0, word.Length);
+            var offsetValues = Enumerable.Range(0, length);
 
             var offsetStrings = outFromTop ?
                 offsetValues.Select(o => new string(letterDistribution[o + row].AsEnumerable().Skip(o).ToArray())) :
-                offsetValues.Select(o => new string(letterDistribution[o + row].AsEnumerable().Skip(word.Length - o - 1).ToArray()));
+                offsetValues.Select(o => new string(letterDistribution[o + row].AsEnumerable().Skip(length - o - 1).ToArray()));
 
-            var offsetWithAdjustedLength = offsetStrings.Select(l => l[..(letterDistribution[0].Length - word.Length + 1)]).ToList();
-            return GetColumns(offsetWithAdjustedLength);
+            var offsetWithAdjustedLength = offsetStrings.Select(l => l[..(letterDistribution[0].Length - length + 1)]).ToList();
+            return Transpose(offsetWithAdjustedLength);
         }
 
-        private static int Scan(string row, string word)
+        private static IEnumerable<string> Transpose(IEnumerable<string> rows)
         {
-            string reversedWord = new(word.Reverse().ToArray());
-
-            var startIndexes = Enumerable.Range(0, row.Length - word.Length + 1);
-            var allSubstrings = startIndexes.Select(s => row.Substring(s, word.Length));
-            var numberOfOccurrences = allSubstrings.Count(s => s == word || s == reversedWord);
-
-            return numberOfOccurrences;
+            IEnumerable<int> columnIndexes = Enumerable.Range(0, rows.First().Length);
+            return columnIndexes.Select(c => new string(rows.Select(l => l[c]).ToArray()));
         }
     }
 }
