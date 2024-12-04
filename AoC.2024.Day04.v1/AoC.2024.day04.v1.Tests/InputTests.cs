3a9b38e98dfa7166fc1324ab04ef6ad32b767602
Reuben <reuben.hillyer@hotmail.co.uk>
2024-12-04 23:39:49 +0000

    Added tests for the input provided for part one

---
+++ AoC.2024.Day04.v1/AoC.2024.day04.v1.Tests/InputTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace AoC._2024.day04.v1.Tests
+{
+    public class InputTests
+    {
+        private readonly string[] inputText = File.ReadAllLines("input.txt");
+
+        [Fact]
+        public void ExampleTextForPartOneReturns2562InstancesOfXMAS()
+        {
+            var wordsearch = new WordSearch(inputText);
+
+            var numberOfOccurrences = wordsearch.FindOccurrencesOf("XMAS");
+
+            numberOfOccurrences.Should().Be(2562);
+        }
+    }
+}
