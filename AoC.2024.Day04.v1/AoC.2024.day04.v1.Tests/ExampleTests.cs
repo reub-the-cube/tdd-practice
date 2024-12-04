bdf5c9efc275f85240a7bbd3501082523b5bab0b
Reuben <reuben.hillyer@hotmail.co.uk>
2024-12-04 19:52:24 +0000

    Initial scaffolding for 2024 day 4

---
+++ AoC.2024.Day04.v1/AoC.2024.day04.v1.Tests/ExampleTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+
+namespace AoC._2024.day04.v1.Tests;
+
+public class ExampleTests
+{
+    private readonly string[] exampleText = File.ReadAllLines("example.txt");
+
+    [Fact]
+    public void InputFileCanBeRead()
+    {
+        exampleText.Should().HaveCount(10);
+    }
+}
