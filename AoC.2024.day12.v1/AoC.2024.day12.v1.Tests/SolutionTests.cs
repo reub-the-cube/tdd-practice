f83c490b877b7ed2bb5e7c57294b46c639c0da4b
Reuben <reuben.hillyer@hotmail.co.uk>
2024-12-16 17:09:19 +0000

    We can solve part one of today's solution

---
+++ AoC.2024.day12.v1/AoC.2024.day12.v1.Tests/SolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace AoC._2024.day12.v1.Tests
+{
+    public class SolutionTests
+    {
+        private readonly string[] inputData = File.ReadAllLines("input.txt");
+
+        [Fact]
+        public void InputDataCalculatesTheTotalFencePriceForPartOne()
+        {
+            var plotMap = new PlotMap(inputData);
+
+            var totalFencePrice = plotMap.TotalFencePrice();
+
+            totalFencePrice.Should().Be(1361494);
+        }
+    }
+}
